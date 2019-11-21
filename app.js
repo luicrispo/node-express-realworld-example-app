051f0d421659a0b3b099f837741c8adfbb759024
luicrispo <57984522+luicrispo@users.noreply.github.com>
2019-11-22 00:23:05 +0400

    Update app.js

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(require('./routes'));
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error('idiota');
   err.status = 404;
   next(err);
 });
