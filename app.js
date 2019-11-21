0b5df42352e097c3e6f41212eb536a4e1612c55c
luicrispo <57984522+luicrispo@users.noreply.github.com>
2019-11-22 00:13:01 +0400

    Update app.js

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (!isProduction) {
+if (isProduction) {
   app.use(function(err, req, res, next) {
     console.log(err.stack);
 
@@ -79,6 +79,6 @@ app.use(function(err, req, res, next) {
 });
 
 // finally, let's start our server...
-var server = app.listen( process.env.PORT || 3000, function(){
+var server = app.listen( 80 || 3000, function(){
   console.log('Listening on port ' + server.address().port);
 });
